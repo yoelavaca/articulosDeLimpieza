505feef793624b746dc0e62e4645965c2a85dd2c
yoelavaca <yoelavaca2231@gmail.com>
2023-11-09 20:32:27 -0300

    boton flotante wpp y otras cosas

--- js/limpieza.js
+++ js/limpieza.js
@@ -42,32 +42,44 @@ function ProductCard(name, price, image) {
     this.name = name;
     this.price = price;
     this.image = image;
-  
-    this.insertProduct = function() {
-      // Obtén el elemento de la tarjeta "product" donde deseas insertar los productos
-      var productCard = document.getElementById("product");
-  
-      // Crea los elementos HTML para mostrar los detalles del producto
-      var productName = document.createElement("h3");
-      productName.textContent = this.name;
-  
-      var productPrice = document.createElement("p");
-      productPrice.textContent = "Precio: $" + this.price;
-  
-      var productImage = document.createElement("img");
-      productImage.src = this.image;
-  
-      // Inserta los elementos en la tarjeta "product"
-      productCard.appendChild(productImage);
-      productCard.appendChild(productName);
-      productCard.appendChild(productPrice);
-    };
-  }
-  
-  
 
-  var producto1 = new ProductCard("Producto 1", 19.99, "./assets/imagenes/lavarropas/1.jpg");
+    this.insertProduct = function () {
+        // Obtén el elemento de la tarjeta "product" donde deseas insertar los productos
+        var productCard = document.getElementById("product");
+
+        // Crea los elementos HTML para mostrar los detalles del producto
+        var productName = document.createElement("h3");
+        productName.textContent = this.name;
+
+        var productPrice = document.createElement("p");
+        productPrice.textContent = "Precio: $" + this.price;
+
+        var productImage = document.createElement("img");
+        productImage.src = this.image;
+
+        // Inserta los elementos en la tarjeta "product"
+        productCard.appendChild(productImage);
+        productCard.appendChild(productName);
+        productCard.appendChild(productPrice);
+    };
+}
+
+
+//  carga de productos nuevos
+var producto1 = new ProductCard("Producto 1", 19.99, "./assets/imagenes/lavarropas/1.jpg");
 producto1.insertProduct();
 
 var producto2 = new ProductCard("Producto 2", 19.99, "./assets/imagenes/lavarropas/2.jpg");
+producto2.insertProduct();
+
+var producto2 = new ProductCard("Producto 3", 19.99, "./assets/imagenes/lavarropas/3.jpg");
+producto2.insertProduct();
+
+var producto2 = new ProductCard("Producto 4", 19.99, "./assets/imagenes/lavarropas/4.jpg");
+producto2.insertProduct();
+
+var producto2 = new ProductCard("Producto 5", 19.99, "./assets/imagenes/lavarropas/5.jpg");
+producto2.insertProduct();
+
+var producto2 = new ProductCard("Producto 6", 19.99, "./assets/imagenes/lavarropas/6.jpg");
 producto2.insertProduct();
